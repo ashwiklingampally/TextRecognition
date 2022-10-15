46261066d1d19ce3fe3057ceedaff308db1dc071
ashwiklingampally <69763261+ashwiklingampally@users.noreply.github.com>
2022-10-15 22:53:17 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:scan_app/text_detector_view.dart';
+
+
+List<CameraDescription> cameras = [];
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  cameras = await availableCameras();
+
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: TextRecognizerView()
+      ),
+    );
+  }
+}
